1a31d63dc595786372853c1951cd71c5d94bbcc0
minseong-dev <aa971020@gmail.com>
2022-01-16 17:54:35 +0900

    add springBoot

---
+++ springBoot/src/test/java/com/example/teddyBuddy/TeddyBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teddyBuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeddyBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
